f5c508c778687486b780b9c1fa63d2ba2fc08003
LeoLacana <wilson17414@gmail.com>
2021-10-31 16:00:02 -0300

    requisito 2

--- src/api/server.js
+++ src/api/server.js
@@ -2,14 +2,27 @@ const {
   registerUsers } = require('../controller/usersController');
 
 const {
-  validationUsers,
-  emailExistValidation } = require('../middleware/usersMiddleware');
+  validationName,
+  validationEmail,
+  validationPassword,
+  validationEmailExist,
+  loginValidation } = require('../middleware/usersMiddleware');
 
 const app = require('./app');
 
 const PORT = 3000;
 
-app.post('/users', validationUsers, emailExistValidation, registerUsers);
+app.post('/users',
+  validationName,
+  validationPassword,
+  validationEmail,
+  validationEmailExist,
+  registerUsers);
+
+app.post('/login',
+  validationEmail,
+  validationPassword,
+  loginValidation);
 
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${PORT}`);
--- src/middleware/usersMiddleware.js
+++ src/middleware/usersMiddleware.js
@@ -1,17 +1,40 @@
+const jwt = require('jsonwebtoken');
 const { showUsers } = require('../service/usersService');
 
-const validationUsers = async (req, res, next) => {
-  const { name, email, password } = req.body;
-  const emailRegex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[com]+/i;
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const validationName = async (req, res, next) => {
+  const { name } = req.body;
   if (!name
-    || !email
-    || !password
-    || !emailRegex.test(email)
     ) return res.status(400).json({ message: 'Invalid entries. Try again.' });
   next();
 };
 
-const emailExistValidation = async (req, res, next) => {
+const validationPassword = async (req, res, next) => {
+  const { password, name } = req.body;
+  if (!password) {
+    if (!name) return res.status(401).json({ message: 'All fields must be filled' });
+    return res.status(400).json({ message: 'Invalid entries. Try again.' });
+  }
+  next();
+};
+
+const validationEmail = async (req, res, next) => {
+  const { email, name } = req.body;
+  const emailRegex = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[com]+/i;
+
+  if (!email || !emailRegex.test(email)) {
+    if (!name) return res.status(401).json({ message: 'All fields must be filled' });
+    
+    return res.status(400).json({ message: 'Invalid entries. Try again.' });
+  }
+  next();
+};
+
+const validationEmailExist = async (req, res, next) => {
   const { email } = req.body;
   const users = await showUsers();
   const emailExist = users.some((user) => user.email === email);
@@ -23,7 +46,22 @@ const emailExistValidation = async (req, res, next) => {
   next();
 };
 
+const loginValidation = async (req, res) => {
+  const { email, password } = req.body;
+  const users = await showUsers();
+  const secret = password;
+  const token = jwt.sign({ data: users.role }, secret, jwtConfig);
+  const emailExist = users.find((user) => user.email === email);
+  if (!emailExist || emailExist.password !== password) {
+    return res.status(401).json({ message: 'Incorrect username or password' });
+  }
+  return res.status(200).json({ token });
+};
+
 module.exports = {
-  validationUsers,
-  emailExistValidation,
+  validationName,
+  validationPassword,
+  validationEmail,
+  validationEmailExist,
+  loginValidation,
 };
